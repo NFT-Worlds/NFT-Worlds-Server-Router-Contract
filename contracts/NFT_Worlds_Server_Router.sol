6c781c3b843dd0ef41b2cc709d5e6ba9a4e9f4b7
Braydon Batungbacal <me@braydonb.com>
2022-01-19 21:57:58 -0800

    Checkpoint.

--- contracts/NFT_Worlds_Server_Router.sol
+++ contracts/NFT_Worlds_Server_Router.sol
@@ -1,10 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract NFT_Worlds_Server_Router {
-  address private nftContract = 0xBD4455dA5929D5639EE098ABFaa3241e9ae111Af;
+/**
+ * The NFT Worlds Server Router contract provides
+ * a decentralized layer to set a JSON blob stored
+ * on IPFS that conforms to the NFT Worlds routing
+ * standards.
+ *
+ * This provides a decentralized way for
+ * NFT World server connection details and other
+ * relevant world information to be set, queried
+ * and distributed.
+ */
 
-  constructor() {
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract NFT_Worlds_Server_Router is Ownable {
+  address private nftWorldsContract;
+
+  address public worldRentalManager;
+  string public convenienceGateway;
+  uint[] public worlds;
+
+  mapping(uint => string) public routingDataIPFSHash;
+
+  constructor(address _nftWorldsContract, address _worldRentalManager, string memory _convenienceGateway) {
+    nftWorldsContract = _nftWorldsContract;
+    worldRentalManager = _worldRentalManager;
+    convenienceGateway = _convenienceGateway;
+  }
+
+  function getRoutingDataURI(uint _worldTokenId, bool includeGateway) external view returns (string memory) {
+    if (includeGateway) {
+      return string(abi.encodePacked(convenienceGateway, routingDataIPFSHash[_worldTokenId]));
+    }
+
+    return string(abi.encodePacked("ipfs://", routingDataIPFSHash[_worldTokenId]));
+  }
+
+  function setRoutingDataIPFSHash(uint _worldTokenId, string calldata _ipfsHash) onlyWorldController(_worldTokenId) external {
+    require(bytes(_ipfsHash).length == 40, "Invalid IPFS Hash");
+
+    routingDataIPFSHash[_worldTokenId] = _ipfsHash;
+  }
+
+  function removeRoutingDataIPFSHash(uint _worldTokenId) onlyWorldController(_worldTokenId) external {
+
+    routingDataIPFSHash[_worldTokenId] = "";
+  }
+
+  function updateWorldRentalManager(address _worldRentalManager) external onlyOwner {
+    worldRentalManager = _worldRentalManager;
+  }
+
+  function updateConvenienceGateway(string calldata _convenienceGateway) external onlyOwner {
+    convenienceGateway = _convenienceGateway;
+  }
+
+  /**
+   * Modifiers
+   */
+
+  modifier onlyWorldController(_worldTokenId) {
+    if (msg.sender != worldRentalManager) {
+      require(true/*TODO*/, "Sender is not world controller.");
+    }
+
+    _;
   }
 }
